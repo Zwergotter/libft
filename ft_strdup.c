65dba9209272a051e8bd05097880ab1a316273bd
Esther DEVEZE <edeveze@student.42.fr>
2017-01-21 16:04:30 +0100

    strdup modified. Now working with strcpy

--- ft_strdup.c
+++ ft_strdup.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edeveze <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: edeveze <edeveze@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/06 20:01:16 by edeveze           #+#    #+#             */
-/*   Updated: 2016/11/11 14:47:38 by edeveze          ###   ########.fr       */
+/*   Updated: 2017/01/21 16:03:50 by edeveze          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,12 @@
 char		*ft_strdup(const char *str)
 {
 	size_t	len;
-	int		i;
 	char	*dest;
 
 	len = ft_strlen(str);
-	i = 0;
 	dest = (char*)malloc(sizeof(*dest) * (len + 1));
 	if (dest == NULL)
 		return (NULL);
-	while (str[i] != '\0')
-	{
-		dest[i] = str[i];
-		i++;
-	}
-	dest[i] = '\0';
+	dest = ft_strcpy(dest, str);
 	return (dest);
 }
